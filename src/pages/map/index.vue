14269094e4907eabcf91eae9d5cbf13cb9361211
poorbug <poorbug@126.com>
2018-05-02 08:19:24 +0800

    feat: join first-stage

--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -3,20 +3,25 @@
     <map
       :markers="markers"
       :polyline="polylines"
+      :controls="controls"
       :include-points="markers"
     />
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import store from '@/store'
 import { TABLE_ID } from '@/constant/'
 import { showErr, calcCenter, calcLines, getCoordinates } from '@/utils/'
 
 export default {
+  store,
   data () {
     return {
       markers: [],
       polylines: [],
+      controls: [],
       people: 0,
       name: ''
     }
@@ -32,13 +37,16 @@ export default {
     }
   },
   mounted () {
-    this.getActivity(this.$root.$mp.query.id)
-    this.getActivityMembers(this.$root.$mp.query.id)
+    this.getActivity()
+    this.getActivityMembers()
+  },
+  computed: {
+    ...mapState([ 'user' ])
   },
   methods: {
-    getActivity (id) {
+    getActivity () {
       let Product = new wx.BaaS.TableObject(TABLE_ID.ACTIVITY)
-      Product.get(id).then(res => {
+      Product.get(this.$root.$mp.query.id).then(res => {
         this.name = res.data.name
         wx.setNavigationBarTitle({
           title: res.data.name
@@ -48,16 +56,36 @@ export default {
     },
     getActivityMembers (id) {
       let query = new wx.BaaS.Query()
-      query.compare('activity_id', '=', id)
+      query.compare('activity_id', '=', this.$root.$mp.query.id)
       let Product = new wx.BaaS.TableObject(TABLE_ID.ACTIVITY_MEMBER)
       Product.setQuery(query).find().then(res => {
         const members = res.data.objects
+        if (members.some(e => (e.user_id === this.user.openid))) {
+          wx.getSystemInfo({
+            success: (res) => {
+              this.controls = [{
+                id: 1,
+                iconPath: require('@/static/image/join.png'),
+                position: {
+                  left: res.windowWidth - 20 - 40,
+                  top: res.windowHeight - 20 - 40,
+                  width: 40,
+                  height: 40
+                },
+                clickable: true
+              }]
+            }
+          })
+        }
         const coors = getCoordinates(members)
         const center = calcCenter(coors)
         this.markers = coors.length === 1 ? coors : coors.concat(center)
         this.polylines = calcLines(coors, center)
       }, () => { showErr('获取成员出错') })
     }
+  },
+  controltap (e) {
+    console.log(e.controlId)
   }
 }
 </script>
